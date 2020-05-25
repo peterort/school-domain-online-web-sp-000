1783c571d63a8278bfcdc720a2b3e0bee332ba95
Peter Ort <peter@peterort.com>
2020-05-25 16:25:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,12 @@
 # code here!
 class School
-  attr_accessor :add_student
 
   def initialize(name)
     def roster
       roster = {}
     end
+  end
+
+  def add_student=(student, grade)
   end
 end
